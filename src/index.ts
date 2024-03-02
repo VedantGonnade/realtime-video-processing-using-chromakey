46c7ca02e3bc7f107b5f15597b5eb0bf4a287405
vedant.gonnade <vedant.gonnade@sportec-solutions.com>
2024-03-02 05:21:50 +0100

    created colourful gradient for canvas 2

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,18 @@ if (!canvasSecondElement) {
   throw new Error("#c1 element not found");
 }
 
-const canvasSecondElementContext: CanvasRenderingContext2D = canvasSecondElement.getContext("2d")!;
+const canvasSecondElementContext: CanvasRenderingContext2D =
+  canvasSecondElement.getContext("2d")!;
 
 if (!canvasSecondElementContext) {
   throw new Error("context cannot be added to the second canvas");
 }
 
-canvasSecondElementContext.fillStyle = "rgb(0, 155, 0)";
+const gradient: CanvasGradient =
+  canvasSecondElementContext.createLinearGradient(0, 0, 250, 50);
+
+gradient.addColorStop(0, "#4741F0");
+gradient.addColorStop(1, "#A647B9");
+
+canvasSecondElementContext.fillStyle = gradient;
 canvasSecondElementContext.fillRect(0, 0, canvasSecondElement.width, canvasSecondElement.height)
