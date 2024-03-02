6ad18cc2bafb73f07598fd26d73dedb9ff15d8be
vedant.gonnade <vedant.gonnade@sportec-solutions.com>
2024-03-02 05:44:11 +0100

    using modular code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-const canvasSecondElement = document.getElementById("c2") as HTMLCanvasElement;
+import { runCanvas } from "./modules/canvasRight";
 
-if (!canvasSecondElement) {
-  throw new Error("#c1 element not found");
-}
-
-const canvasSecondElementContext: CanvasRenderingContext2D =
-  canvasSecondElement.getContext("2d")!;
-
-if (!canvasSecondElementContext) {
-  throw new Error("context cannot be added to the second canvas");
-}
-
-const gradient: CanvasGradient =
-  canvasSecondElementContext.createLinearGradient(0, 0, 250, 50);
-
-gradient.addColorStop(0, "#4741F0");
-gradient.addColorStop(1, "#A647B9");
-
-canvasSecondElementContext.fillStyle = gradient;
-canvasSecondElementContext.fillRect(0, 0, canvasSecondElement.width, canvasSecondElement.height)
+runCanvas();
