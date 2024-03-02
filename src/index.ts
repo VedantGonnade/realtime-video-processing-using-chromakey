8ec210de489e7773c0545f91a14cbc2f24ce5b2b
vedant.gonnade <vedant.gonnade@sportec-solutions.com>
2024-03-02 04:51:43 +0100

    generate green background on canvas 2 using TS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hell Vedantnd");
+const canvasSecondElement = document.getElementById("c2") as HTMLCanvasElement;
+
+if (!canvasSecondElement) {
+  throw new Error("#c1 element not found");
+}
+
+const canvasSecondElementContext = canvasSecondElement.getContext("2d")!;
+
+if (!canvasSecondElementContext) {
+  throw new Error("context cannot be added to the second canvas");
+}
+
+canvasSecondElementContext.fillStyle = "rgb(0, 155, 0)";
+canvasSecondElementContext.fillRect(0, 0, canvasSecondElement.width, canvasSecondElement.height)
